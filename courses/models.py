fd658994ccd798eadead65a705b93012c202cd52
Alexandr Rybakov <rybakov336@yandex.ru>
2024-02-27 14:38:03 +0300

    completed crud drf

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from config import settings
+
 NULLABLE = {'blank': True, 'null': True}
 
 
@@ -7,6 +9,7 @@ class Course(models.Model):
     title = models.CharField(max_length=255, verbose_name='курс')
     preview = models.ImageField(max_length=255, upload_to='course', verbose_name='превью', **NULLABLE)
     description = models.TextField(max_length=255, verbose_name='описание', **NULLABLE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, **NULLABLE)
 
     def __str__(self):
         return f'{self.title}'
@@ -22,6 +25,7 @@ class Lesson(models.Model):
     description = models.TextField(max_length=255, **NULLABLE, verbose_name='описание')
     course = models.ForeignKey(Course, on_delete=models.CASCADE, **NULLABLE, verbose_name='курс')
     video_url = models.URLField(**NULLABLE, verbose_name='ссылка на видео')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, **NULLABLE)
 
     def __str__(self):
         return f'{self.title}'
